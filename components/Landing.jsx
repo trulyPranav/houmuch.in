af1d945319eb14324dc04da7dea0a9104dc5048d
Pranav M <pranavm265@gmail.com>
2025-03-14 00:15:53 +0530

    works?

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -9,10 +9,10 @@ const Landing = () => {
                 <p className="text-black text-5xl font-[700] font-inter leading-snug">at lowest rates!</p>
                 <p className="text-black text-5xl font-[700] font-inter leading-snug">Download Now!!</p>
                 <div className="flex mt-8 justify-stretch w-3/4">
-                    <button className="px-[40] py-[15] bg-[#FF6D2C] text-white text-[14px] font-inter border-2 border-black border-b-[5px] border-r-[5px] rounded-[13px]">
+                    <button className="px-10 py-4 bg-[#FF6D2C] text-white text-[14px] font-inter border-2 border-black border-b-[5px] border-r-[5px] rounded-[13px]">
                         EXPLORE MORE
                     </button>
-                    <div className="pl-[50] flex flex-col justify-center items-baseline">
+                    <div className="pl-12 flex flex-col justify-center items-baseline">
                         <img src="/LandingAssets/fiveStars.png" alt="Five Stars!" className="w-[70%] h-auto" />
                         <p className="text-black font-inter font-[500] text-sm">5.0 Shopify Rating</p>
                     </div>
